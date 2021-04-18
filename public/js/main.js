a70603e934066d942aa9faac1709428a368d747f
casper-hansen <repsac2002@gmail.com>
2021-04-18 21:34:50 +0200

    Added timeout after 10 wrong password tries >:D

--- public/js/main.js
+++ public/js/main.js
@@ -3,19 +3,28 @@ var loginScreen = document.querySelector(".loginScreen")
 var signupBtn = document.querySelector(".signupButton") 
 var signupScreen = document.querySelector(".signupScreen")
 var fadeLayer = document.querySelector(".fadeLayer")
-
-
-loginBtn.addEventListener("click", function(){
-    loginScreen.classList.remove("hide")
-    signupScreen.classList.add("hide")
-});
-signupBtn.addEventListener("click", function(){
-    signupScreen.classList.remove("hide")
-    loginScreen.classList.add("hide")
-});
-
-fadeLayer.addEventListener("click", function(){
-    console.log("hiding shit")
-    signupScreen.classList.add("hide")
-    loginScreen.classList.add("hide")
-})
+var linkTimeout = document.querySelector(".tooMany")
+try{
+    loginBtn.addEventListener("click", function(){
+        loginScreen.classList.remove("hide")
+        signupScreen.classList.add("hide")
+    });
+}catch(e){console.log(e)}
+try{
+    signupBtn.addEventListener("click", function(){
+        signupScreen.classList.remove("hide")
+        loginScreen.classList.add("hide")
+    });
+}catch(e){console.log(e)}
+try{
+    fadeLayer.addEventListener("click", function(){
+        console.log("hiding shit")
+        signupScreen.classList.add("hide")
+        loginScreen.classList.add("hide")
+    })
+}catch(e){console.log(e)}
+try{
+    setTimeout(() => {
+        linkTimeout.classList.remove("hide")
+    }, 30000)
+}catch(e){console.log(e)}
